78dfa7d3d2fb81b09234f97f436d4458ffe11eb6
Carlos Eduardo Inca Cruz <72511325+CarlosEdu322@users.noreply.github.com>
2021-02-07 18:32:47 -0500

    dbo

--- AppControlPlanTesis/LibFormularios/FrmIniciarTramiteEstudiante.Designer.cs
+++ AppControlPlanTesis/LibFormularios/FrmIniciarTramiteEstudiante.Designer.cs
@@ -30,6 +30,9 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.CodTesista2 = new System.Windows.Forms.GroupBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.TxtCodTesista3 = new System.Windows.Forms.TextBox();
             this.LblCodTesista3 = new System.Windows.Forms.Label();
             this.TxtCodTesista2 = new System.Windows.Forms.TextBox();
@@ -42,9 +45,8 @@
             this.LblNroEstudiantes = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
             this.ChlRequisitosXTramite = new System.Windows.Forms.CheckedListBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
             this.CodTesista2.SuspendLayout();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
@@ -52,7 +54,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(341, 21);
+            this.label1.Location = new System.Drawing.Point(289, 28);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(94, 17);
             this.label1.TabIndex = 0;
@@ -75,6 +77,33 @@
             this.CodTesista2.TabIndex = 1;
             this.CodTesista2.TabStop = false;
             this.CodTesista2.Text = "Codigo De Estudiante 1";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(546, 127);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(57, 27);
+            this.button3.TabIndex = 159;
+            this.button3.Text = "...";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(546, 84);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(57, 27);
+            this.button2.TabIndex = 158;
+            this.button2.Text = "...";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(546, 34);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(57, 27);
+            this.button1.TabIndex = 157;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // TxtCodTesista3
             // 
@@ -202,38 +231,35 @@
             this.ChlRequisitosXTramite.Size = new System.Drawing.Size(409, 174);
             this.ChlRequisitosXTramite.TabIndex = 145;
             // 
-            // button1
+            // comboBox1
             // 
-            this.button1.Location = new System.Drawing.Point(546, 34);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(57, 27);
-            this.button1.TabIndex = 157;
-            this.button1.Text = "...";
-            this.button1.UseVisualStyleBackColor = true;
+            this.comboBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(189, 369);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(411, 30);
+            this.comboBox1.TabIndex = 155;
             // 
-            // button2
+            // label2
             // 
-            this.button2.Location = new System.Drawing.Point(546, 84);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(57, 27);
-            this.button2.TabIndex = 158;
-            this.button2.Text = "...";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(546, 127);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(57, 27);
-            this.button3.TabIndex = 159;
-            this.button3.Text = "...";
-            this.button3.UseVisualStyleBackColor = true;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 10.8F);
+            this.label2.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.label2.Location = new System.Drawing.Point(60, 366);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(121, 22);
+            this.label2.TabIndex = 154;
+            this.label2.Text = "CodTramite:";
             // 
             // FrmIniciarTramiteEstudiante
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(696, 798);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.LblNroEstudiantes);
             this.Controls.Add(this.CboNroEstudiantes);
@@ -270,5 +296,7 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        protected System.Windows.Forms.Label label2;
     }
 }
